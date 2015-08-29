543499c7b4823c0fa863cce0a472d61345c5ea8e
Kim Lavoie <kim.lavoie.14@gmail.com>
2015-08-28 22:31:28 -0400

    Removed useless line in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,5 @@
 sudo apt-get -y update
 sudo apt-get install -y curl
 curl https://install.meteor.com/ | sudo sh
-export MONGO_URL=mongodb://localhost:27017/app
 sudo cp /vagrant/launch.sh /usr/bin/launch
 sudo chmod +x /usr/bin/launch
